cf2b167f5f5816e2b8a3814a39d6b39501eb6bcf
saecula <krgilb@gmail.com>
2019-01-05 09:59:26 -0500

    one more travis test

--- client/components/User.jsx
+++ client/components/User.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default (props) => (
   <tr>
-    <td><h3>woooo00oo00ooottt</h3></td>
+    <td><h3>wAAAAttt</h3></td>
     <td className="user--first-name">{props.firstName}</td>
     <td className="user--last-name">{props.lastName}</td>
     <td className="user--email">{props.email}</td>
