9bc06167f3ef2b17e740278dee196205c4b53e2d
saecula <krgilb@gmail.com>
2019-01-05 09:32:12 -0500

    test test

--- client/components/User.jsx
+++ client/components/User.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default (props) => (
   <tr>
     <td className="user--first-name">{props.firstName}</td>
-    <td className="user--last-name">{props.lastName}</td>
+    <td className="user--last-name">{props.firstName}</td>
     <td className="user--email">{props.email}</td>
   </tr>
 )
