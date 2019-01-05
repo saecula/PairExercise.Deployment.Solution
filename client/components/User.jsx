6ae2d4c1c6984023800e76594261885afcbd34c8
saecula <krgilb@gmail.com>
2019-01-05 09:34:00 -0500

    trying to change User.jsx

--- client/components/User.jsx
+++ client/components/User.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default (props) => (
   <tr>
+    <td><h3>wooooooooottt</h3></td>
     <td className="user--first-name">{props.firstName}</td>
     <td className="user--last-name">{props.firstName}</td>
     <td className="user--email">{props.email}</td>
