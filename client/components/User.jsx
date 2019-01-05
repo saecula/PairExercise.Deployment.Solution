0f5ad83e0df94e527a8454f2bb742fa7fa5552f8
saecula <krgilb@gmail.com>
2019-01-05 09:37:14 -0500

    still deploy

--- client/components/User.jsx
+++ client/components/User.jsx
@@ -4,7 +4,7 @@ export default (props) => (
   <tr>
     <td><h3>wooooooooottt</h3></td>
     <td className="user--first-name">{props.firstName}</td>
-    <td className="user--last-name">{props.firstName}</td>
+    <td className="user--last-name">{props.lastName}</td>
     <td className="user--email">{props.email}</td>
   </tr>
 )
